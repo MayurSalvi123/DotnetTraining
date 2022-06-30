ba2d8132b26673353548a15084f289dcc9ec42c1
MayurSalvi123 <msalvi660@gmail.com>
2022-06-30 23:11:27 +0530

    Did Changed As Guided and added xUnit Tests

--- PasswordVerifier/Program.cs
+++ PasswordVerifier/Program.cs
@@ -1,19 +1,4 @@
-﻿using PasswordVerifier;
-
-try
-{
-    //Verify("qwe123");
-    //Verify(null);
-    //Verify("");
-    //Verify("Qwe123");
-    //Verify("jdfdfsdjffj1");
-    //Verify("SDNFKSDFKSDKSD1");
-    //  Verify("24456456165616443");
-    Verify.VerifyPassword("Password123");
-    
-}
-catch (Exception e)
+﻿static void Main()
 {
 
-    Console.WriteLine(e.Message);
 }
--- PasswordVerifier/Verify.cs
+++ PasswordVerifier/Verify.cs
@@ -4,27 +4,23 @@ namespace PasswordVerifier
 {
     public class Verify
     {
-        public static void VerifyPassword(string password)
+        private static readonly Regex hasNumber = new(@"[0-9]");
+        private static readonly Regex hasLowerChar = new(@"[a-z]");
+        private static readonly Regex hasUpperChar = new(@"[A-Z]");
+        private static readonly Regex hasMinimumChars = new(@".{9,}");
+        public static void VerifyPassword(string? password)
         {
             if (string.IsNullOrEmpty(password))
                 throw new Exception("Password is Null or Empty");
-            var hasNumber = new Regex(@"[0-9]");
-            var hasLowerChar = new Regex(@"[a-z]");
-            var hasUpperChar = new Regex(@"[A-Z]");
-            var hasMinimum8Chars = new Regex(@".{8,}");
-
-            //var isValidated = hasNumber.IsMatch(password) && hasUpperChar.IsMatch(password) && hasMinimum8Chars.IsMatch(password) && hasLowerChar.IsMatch(password);
-
+            if (!hasMinimumChars.IsMatch(password))
+                throw new Exception("Password Does Not Contain Minimum of 9 Characters");           
             if (!hasNumber.IsMatch(password))
                 throw new Exception("Password Does Not Contain Number");
             if (!hasUpperChar.IsMatch(password))
                 throw new Exception("Password Does Not Contain Upper Case Characters");
-            if (!hasMinimum8Chars.IsMatch(password))
-                throw new Exception("Password Does Not Contain Minimum of 8 Characters");
             if (!hasLowerChar.IsMatch(password))
                 throw new Exception("Password Does Not Contain Lower Case Characters");
-            //if (isValidated)
-            Console.WriteLine("Password validated");
+            Console.Write("Password validated");
         }
     }
 }
